56364e7757b98e9cddb945f574cbc5e8680814b5
Rodrigo Andrade <soadse.ra@gmail.com>
2024-12-10 21:24:42 -0300

    teste

---
+++ js/arcanoPessoal.js
@@ -0,0 +1,11 @@
+let bd = document.body
+let data = document.getElementById('data')
+let resultado = document.getElementById('resultado')
+let calcular = document.getElementById('calcular')
+
+calcular.addEventListener('click', () => {
+    for(i=0; i<=data; i++){
+        resultado.innerHTML = 'oi'
+    }
+})
+
