8452e514e5fe265c60135ad15a6763f536ca6a2a
Rodrigo Andrade <soadse.ra@gmail.com>
2024-12-11 21:01:35 -0300

    teste

--- js/arcanoPessoal.js
+++ js/arcanoPessoal.js
@@ -1,11 +1,12 @@
 let bd = document.body
-let data = document.getElementById('data')
+let dia = document.getElementById('dia').value
+let mes = document.getElementById('mes')
+let ano = document.getElementById('ano')
 let resultado = document.getElementById('resultado')
 let calcular = document.getElementById('calcular')
 
 calcular.addEventListener('click', () => {
-    for(i=0; i<=data; i++){
-        resultado.innerHTML = 'oi'
+    for(i = 0; i <= dia.length; i++){
+        resultado.innerHTML += va
     }
 })
-
